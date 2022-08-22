dafcd5488a9fcc07e18be6d165040bd472fd5e52
adrvapor <adr.vaquero@gmail.com>
2022-08-22 20:55:33 +0200

    Api and Web projects created

---
+++ PPOC.Api/Database/PPOCApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PPOC.Api.Models;
+
+namespace PPOC.Api.Database
+{
+    public class PPOCApiDbContext : DbContext
+    {
+        public PPOCApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
